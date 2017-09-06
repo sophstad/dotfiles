a4af1a92d5be682046694f2650cc8a2dcdd711a1
Sophie Stadler <srs2231@columbia.edu>
2017-09-06 01:00:08 -0400

    Initial commit

---
+++ Brewfile
@@ -0,0 +1,17 @@
+cask_args appdir: '/Applications'
+
+# install commandline utils
+brew 'coreutils'
+brew 'grc'
+brew 'node'
+brew 'tmux'
+brew 'vim'
+brew 'wget'
+
+# install apps
+cask 'dropbox'
+cask 'flux'
+cask 'google-chrome'
+cask 'slack'
+cask 'spotify'
+cask 'sublime-text'
---
+++ macos/set-defaults.sh
@@ -0,0 +1,9 @@
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
